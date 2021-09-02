f8041d589416d470f5510139b6dc8ed2ddee50d7
ibntofajjal <joynal@bintofajjal.info>
2021-09-02 13:35:50 +0600

    Add error Handler

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
-// 》STARTING JAVASCRIPT《
+// ㊋ █▓▓▓▓▓▓▓▓▓█ STARTING JAVASCRIPT █▓▓▓▓▓▓▓▓▓█ ㊋
 const loadData = () => {
   const searchField = document.getElementById("searchField"); // Selecting Search Input By DOM
   const searchText = searchField.value; // Take the value from Search Input
+  searchField.value = "";
   const url = `https://openlibrary.org/search.json?q=${searchText}`; // API-JSON
   fetch(url)
     .then((res) => res.json())
@@ -9,24 +10,33 @@ const loadData = () => {
 };
 
 const displayBooks = (books) => {
-  console.log(books);
+  // console.log(books);
   const booksContainer = document.getElementById("booksContainer");
-  for (const book of books) {
-    books.forEach((book) => {
-      const bookDiv = document.createElement("div");
-      const img = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
-      bookDiv.classList.add("col");
-      bookDiv.innerHTML = `
+  // Error Handling
+  if (books.length === "" || books.length === 0) {
+    const errorMsg = document.getElementById("errorMsg");
+    errorMsg.innerHTML = `
+    <h1>Please type anythig first</h1>`;
+    booksContainer.appendChild(h1);
+  }
+
+  booksContainer.textContent = ""; // Clear the previous result
+  books.forEach((book) => {
+    const bookDiv = document.createElement("div");
+    const img = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+    bookDiv.classList.add("col");
+    bookDiv.innerHTML = `
     <div class="card h-100 text-dark">
     <img class="img-fluid" src="${img}" class="card-img-top" alt="...">
       <div class="card-body">
-          <h5 class="card-title">${book.title}</h5>
-          <p class="card-text">ABCS</p>
-          <button class="btn btn-success">Show Food Details</button>
+          <h5 class="card-title">Book Name: <b>${book.title}</b></h5>
+          <p class="card-text">Author Name: <b>${book.author_name[0]}</b></p>
+          <p class="card-text">Author Alternativ Name: <b>${book.author_alternative_name[0]}</b></p>
+          <p class="card-text">Book Publisher: <b>${book.publisher[0]}</b></p>
+          <p class="card-text">First Publish Year: <b>${book.first_publish_year}</b></p>
       </div>
     </div>
     `;
-      booksContainer.appendChild(bookDiv);
-    });
-  }
+    booksContainer.appendChild(bookDiv);
+  });
 };
