31c81b54351af80d4246327ec522998eef32070b
ibntofajjal <joynal@bintofajjal.info>
2021-09-02 20:33:17 +0600

    Complete the project

--- js/app.js
+++ js/app.js
@@ -1,42 +1,69 @@
-// ㊋ █▒▒▒▒▒▒▒▒▒█ STARTING JAVASCRIPT █▒▒▒▒▒▒▒▒▒█ ㊋
+//  █▒▒▒▒▒▒▒▒▒《 LOAD/FETCHING DATA FROM API 》▒▒▒▒▒▒▒▒▒█
 const loadData = () => {
-  const searchField = document.getElementById("searchField"); // Selecting Search Input By DOM
-  const searchText = searchField.value; // Take the value from Search Input
+  // Selecting Search Input By DOM
+  const searchField = document.getElementById("searchField");
+  // Take the value from Search Input
+  const searchText = searchField.value;
   searchField.value = "";
-  const url = `https://openlibrary.org/search.json?q=${searchText}`; // API-JSON
+
+  // API-JSON
+  const url = `https://openlibrary.org/search.json?q=${searchText}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displayBooks(data.docs));
+    .then((data) => displayBooks(data));
 };
-//  █▒▒▒▒▒▒▒▒▒▶ DISPLAYING RESULS ◀▒▒▒▒▒▒▒▒▒█
+
 const displayBooks = (books) => {
-  // console.log(books);
+  const api = books.docs;
   const booksContainer = document.getElementById("booksContainer");
-  // Error Handling
-  if (books.length === "" || books.length === 0) {
-    const errorMsg = document.getElementById("errorMsg");
-    errorMsg.innerHTML = `
-    <h1>Please type anythig first</h1>`;
-    booksContainer.appendChild(h1);
+  // Result Found
+  const showResult = books.numFound;
+  if (showResult === 0) {
+    const displySearchResult = document.getElementById("result-found");
+    // clear Data
+    displySearchResult.textContent = "";
+    const div = document.createElement("div");
+    div.classList.add("h2");
+    div.innerHTML = `Result Not Found <h3>${showResult}</h3> `;
+    displySearchResult.appendChild(div);
+  } else {
+    const displySearchResult = document.getElementById("result-found");
+    // clear Data
+    displySearchResult.textContent = "";
+    const div = document.createElement("div");
+    div.classList.add("h2");
+    div.innerHTML = `Ressult Found <h3>${showResult}</h3> `;
+    displySearchResult.appendChild(div);
   }
 
-  booksContainer.textContent = ""; // Clear the previous result
-  books.forEach((book) => {
-    const bookDiv = document.createElement("div");
-    const img = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
-    bookDiv.classList.add("col");
-    bookDiv.innerHTML = `
-    <div class="card h-100 text-dark">
-    <img class="img-fluid" src="${img}" class="card-img-top" alt="...">
-      <div class="card-body">
-          <h5 class="card-title">Book Name: <b>${book.title}</b></h5>
-          <p class="card-text">Author Name: <b>${book.author_name[0]}</b></p>
-          <p class="card-text">Author Alternativ Name: <b>${book.author_alternative_name[0]}</b></p>
-          <p class="card-text">Book Publisher: <b>${book.publisher[0]}</b></p>
-          <p class="card-text">First Publish Year: <b>${book.first_publish_year}</b></p>
+  // Error Handling
+  if (api.length === "" || api.length === 0) {
+    const errorMsg = document.getElementById("errorMsg");
+    errorMsg.innerHTML = `
+    <h1 class="text-center">Please type a valid word</h1>`;
+    booksContainer.appendChild(h1);
+  } else {
+    // Clear the previous error message
+    errorMsg.innerHTML = ``;
+    // Clear the previous result
+    booksContainer.textContent = "";
+    // Looping For Get The Book List Via Search result
+    api.forEach((book) => {
+      const bookDiv = document.createElement("div");
+      const img = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+      bookDiv.classList.add("col");
+      bookDiv.innerHTML = `
+      <div class="card h-100 text-dark">
+      <img class="img-fluid" src="${img}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Book Name: <b>${book.title}</b></h5>
+            <p class="card-text">Author Name: <b>${book.author_name[0]}</b></p>
+            <p class="card-text">Book Publisher: <b>${book.publisher[0]}</b></p>
+            <p class="card-text">First Publish Year: <b>${book.first_publish_year}</b></p>
+        </div>
       </div>
-    </div>
-    `;
-    booksContainer.appendChild(bookDiv);
-  });
+      `;
+      booksContainer.appendChild(bookDiv);
+    });
+  }
 };
---
+++ readme.md
@@ -0,0 +1,17 @@
+<!-- ████████████████████████████████████████████████████████ -->
+
+### For Main part
+
+1. Book name ✅
+2. Author name ✅
+3. Book Publisher ✅
+4. Publishing Year ✅
+5. How many search results have you given? (SHOWING ON THE BOTTOM OF SEARCH FIELD)
+6. Error Handle for Unexpected input ✅
+7. Book Image ✅
+
+### For Bonus par
+
+1. Using Arrow Function ✅
+2. Usign ForEach Loop ✅
+3. Comments For Readable Code ✅
